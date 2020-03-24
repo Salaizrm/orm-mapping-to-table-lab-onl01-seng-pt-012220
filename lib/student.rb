9de8d5ac0fdfef8c6332352fe49de7a91e0aa698
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-24 23:08:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     sql = <<-SQL
     INSERT INTO students (id, name, grade) VALUES (?,?,?)
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, self.id self.name, self.grade)
   end
 
   # Remember, you can access your database connection anywhere in this class
