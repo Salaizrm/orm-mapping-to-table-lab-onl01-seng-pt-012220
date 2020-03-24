976f9dd40a17fe02bf6178f9bda41dfbe68e738f
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-24 23:04:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,9 +31,9 @@ class Student
 
   def save
     sql = <<-SQL
-    INSERT INTO students(name,grade)
+    INSERT INTO students(name,grade) VALUES (?,?)
     SQL
-
+    DB[:conn].execute(sql)
   end
 
   # Remember, you can access your database connection anywhere in this class
