15d5cd3cea5053a3e305ca711173f15531211379
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-24 22:54:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,11 @@ class Student
 
   def self.
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS song
+    CREATE TABLE IF NOT EXISTS student (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    );
 
     SQL
   end
