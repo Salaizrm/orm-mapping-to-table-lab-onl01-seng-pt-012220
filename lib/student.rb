88ee012ed2a55137aa05171951da7fecd330a734
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-24 23:12:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,9 @@ class Student
   end
 
   def self.create
-
+    students = self.new(name, grade)
+    student.save
+    student
   end
 
   # Remember, you can access your database connection anywhere in this class
