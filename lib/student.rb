d4f45df5922c82e47ceb8813408f41785a9e535e
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-24 22:58:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,18 @@ class Student
     @grade = grade
   end
 
+  def create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS student (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
 
 
 
